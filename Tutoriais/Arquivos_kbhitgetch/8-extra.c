fc06423c912d62c355bb1dc188b33de59459fe1e
CarlosAdir <carlos.adir.leite@gmail.com>
2017-09-06 15:58:26 -0300

    add some keys

--- Tutoriais/Arquivos_kbhitgetch/8-extra.c
+++ Tutoriais/Arquivos_kbhitgetch/8-extra.c
@@ -8,6 +8,11 @@ int main()
 		buffer_clear();
 		t = get_key();
 		printf("Foi apertado %s\n", key_to_str(t));
+		/* As possibilidades sao:
+			ENTER, ESC, BACKSPACE, F2, F3, F4, F5, F6, INSERT
+			DELETE, PGUP, PGDN, HOME, END
+			UP_KEY, DOWN_KEY, RIGHT_KEY, LEFT_KEY
+		*/
 	}while(!key_is_equal(t, "ENTER"));
 	return 0;
 }
